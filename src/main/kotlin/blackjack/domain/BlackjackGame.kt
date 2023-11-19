5a93f4a8cf9176dc01086756eaeefe05dfa427e9
dokdogalmaegi <clik202012@gmail.com>
2023-11-19 22:38:35 +0900

    Remove & Impl: start function & procee function

--- src/main/kotlin/blackjack/domain/BlackjackGame.kt
+++ src/main/kotlin/blackjack/domain/BlackjackGame.kt
@@ -10,7 +10,12 @@ class BlackjackGame(private var playerList: List<Player>) {
         require(playerList.isNotEmpty()) { "플레이어가 존재하지 않습니다." }
     }
 
-    fun start(): List<Player> {
-        TODO("Not yet implemented")
+    fun process(player: Player): Player {
+        require(player.isPlay()) { "이미 게임이 종료된 플레이어입니다." }
+
+        val playerSnapshot: Player = player.getSnapshot()
+        playerSnapshot.addCard(cardDealer.dealCard())
+
+        return playerSnapshot
     }
 }
